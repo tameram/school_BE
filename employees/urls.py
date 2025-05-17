6166249752cebe2f50c241a97101191fd9068a39
tameram <tamer.dweat@gmail.com>
2025-05-17 22:42:51 +0300

    fix urls

--- employees/urls.py
+++ employees/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import EmployeeListCreateView, EmployeeRetrieveUpdateView, EmployeeHistoryViewSet
+from .views import EmployeeListCreateView, EmployeeRetrieveUpdateView
 
 
 urlpatterns = [
     path('', EmployeeListCreateView.as_view(), name='employee-list-create'),
-    path('employee-history', EmployeeHistoryViewSet, basename='employee-history'),
     path('<uuid:id>/', EmployeeRetrieveUpdateView.as_view(), name='employee-detail-update'),
 ]
