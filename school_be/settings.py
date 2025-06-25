90b6e078f8a10cef8ff42aba590641e702c0ae44
tameram <tamer.dweat@gmail.com>
2025-06-25 23:34:58 +0300

    modify the confg

--- school_be/settings.py
+++ school_be/settings.py
@@ -26,7 +26,23 @@ SECRET_KEY = 'django-insecure-4=l&9nvv*^rr$vugaemivz^sq=+(e(ijyetcbu*i!v(t@!)9!$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['daftar-noon.com', 'www.daftar-noon.com', '51.16.24.229', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'https://daftar-noon.com',
+    'https://www.daftar-noon.com',
+    'http://daftar-noon.com',  # Keep HTTP for now
+    'http://www.daftar-noon.com',
+]
+
+ALLOWED_HOSTS = [
+    'daftar-noon.com', 
+    'www.daftar-noon.com', 
+    '51.16.24.229', 
+    'localhost', 
+    '127.0.0.1'
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = False
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -80,13 +96,14 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://daftar-noon.com",
-    "http://daftar-noon.com",
-    "https://www.daftar-noon.com",
-    "http://www.daftar-noon.com",
-    "http://localhost:3000",
-    "http://51.16.24.229",
+    'https://daftar-noon.com',
+    'https://www.daftar-noon.com',
+    'http://daftar-noon.com',
+    'http://www.daftar-noon.com',
+    'http://localhost:3000',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
